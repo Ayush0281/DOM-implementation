54b3ca7835a37c8f35f57b92e062e998da960518
Sarthak Rana <151067831+Ayush0281@users.noreply.github.com>
2026-03-09 01:47:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const redBox = document.getElementById("red");
+const blueBox = document.getElementById("blue");
+const greenBox = document.getElementById("green");
+const yellowBox = document.getElementById("yellow");
+
+
+redBox.onclick = function() {
+    redBox.style.backgroundColor = "red";
+}
+
+blueBox.onclick = function(){
+    blueBox.style.backgroundColor = "blue";
+}
+
+greenBox.onclick = function(){
+    greenBox.style.backgroundColor = "green";
+}
+
+yellowBox.onclick = function(){
+    yellowBox.style.backgroundColor = "yellow";
+}
+
+
+function greetUser() {
+    const name = document.getElementById("nameInput").value;
+
+    const greeting = document.getElementById("greeting");
+
+    greeting.innerText = "Hello, " + name;
+
+}
+
+
